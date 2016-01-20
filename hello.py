e6de37773776464d5e4966bb5580ea3f6e51d141
brahmramkissoon <brahm.ramkissoon@gmail.com>
2016-01-20 23:33:54 +0000

    index returns url_for show_user_profile

--- hello.py
+++ hello.py
@@ -1,26 +1,21 @@
 import os
 
 
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Index Page"
+    return url_for('show_user_profile', username='david')
     
-@app.route('/user/<username>')
+@app.route('/username/<username>')
 def show_user_profile(username):
-    visits = 3
     # show the user profile for that user
-    return "User %s visited %d times ( 3 just a sample here)" % (username, visits)
-    
-@app.route('/hello')
-def hello_world():
-    return "Hello World"
+    return "User %s" % username
     
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
-    # show th epost with the given id, the ide must be an integer
+    # show the post with the given id, the ide must be an integer
     return "Post %d" % post_id
 
 if __name__ == '__main__':
