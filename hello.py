13e5af0cad80f5040230c9ad036cd67269fc2d40
brahmramkissoon <brahm.ramkissoon@gmail.com>
2016-01-21 02:24:03 +0000

    added login.html and wireup

--- hello.py
+++ hello.py
@@ -1,17 +1,15 @@
 import os
 
-
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-# view a login page using GET, pass the username using POST
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login_user', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        return 'username is ' + request.values["username"]
-    else:
-        return '<form method="post" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></p></form>'
-
+    # user has pressed submit button
+    if request.method == 'POST': 
+        return "User %s logged in" % request.form['username']
+    return render_template('login.html')
+    
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
